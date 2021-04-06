8d390474c2b722387eb2e3e921699e9ab4a2d804
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-04-06 16:42:58 -0600

    Add files via upload
    Finishing touches to the lyric generator

--- week10/app.js
+++ week10/app.js
@@ -5,10 +5,17 @@ const port = process.env.PORT || 5000;
 const axios = require('axios');
 const { Pool } = require('pg');
 
-
-const connectionString = process.env.DATABASE_URL || 'postgres://famuwdcambmyum:95c91a12ed40739ed33d91ff68c928179c526ac319fcb20221f3695fb21bcc2a@ec2-18-233-83-165.compute-1.amazonaws.com:5432/d8ru6l43duhil?ssl=true';
-const pool = new Pool({ connectionString: connectionString });
+const db_url = 'postgres://famuwdcambmyum:95c91a12ed40739ed33d91ff68c928179c526ac319fcb20221f3695fb21bcc2a@ec2-18-233-83-165.compute-1.amazonaws.com:5432/d8ru6l43duhil?ssl=true';
+const connectionString = process.env.DATABASE_URL || db_url;
+const pool = new Pool({
+    connectionString: db_url,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+//const pool = new Pool({ connectionString: connectionString });
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 
 
 pool.on('error', (err, client) => {
@@ -39,6 +46,7 @@ app.get('/', homePage);
 app.get('/home', homePage);
 app.get('/mySongs', mySongs);
 app.get('/addMySong', addMySong);
+app.get('/goToFavorites', goToFav);
 
 
 app.listen(port, () => {
@@ -54,6 +62,7 @@ function mySongs(req, res) {
 }
 
 
+
 async function addMySong(req, res) {
     var author = req.query.authorInput;
     var songTitle = req.query.songNameInput;
@@ -62,86 +71,52 @@ async function addMySong(req, res) {
     console.log('title: ' + songTitle);
     //console.log('lyrics: ' + songLyrics);
 
-    // var checkDuplicateStmt = "SELECT song_title FROM mySongs WHERE song_title = 'back in black';";
-    // //var checkValues = [songTitle];
-    // pool.query(checkDuplicateStmt, (err, res) => {
-    //     console.log('this is req.rows[0]' + req.rows[0])
-    //     if (err) {
-    //         console.log(err)
-    //     } else {
-    //         if (req.rows == null) {
-    //             console.log('Value of req.rows' + req.rows)
-    var insertStmt = "INSERT INTO mySongs (author, song_title, lyrics) VALUES($1, $2, $3);"
-    var bindVariables = [author, songTitle, songLyrics];
-    pool.query(insertStmt, bindVariables, (err, res) => {
-        console.log(err);
+    var checkDuplicateStmt = "SELECT song_title FROM mySongs WHERE song_title = $1;";
+    var checkDuplicateBindVars = [songTitle];
+    //var checkValues = [songTitle];
+    pool.query(checkDuplicateStmt, checkDuplicateBindVars, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (result.rows[0] == null) {
+                var insertStmt = "INSERT INTO mySongs (author, song_title, lyrics) VALUES($1, $2, $3);"
+                var bindVariables = [author, songTitle, songLyrics];
+                pool.query(insertStmt, bindVariables, (err, result) => {
+                    console.log(err);
+                })
+
+                pool.query("SELECT song_title, author, lyrics FROM mySongs;", (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var rows = result.rows;
+                        res.render('mySongs', { params: JSON.stringify(rows) });
+                    }
+                })
+            }
+            else {
+                console.log(result.rows[0])
+                pool.query("SELECT song_title, author, lyrics FROM mySongs;", (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var rows = result.rows;
+                        res.render('mySongs', { params: JSON.stringify(rows) });
+                    }
+                })
+            }
+        }
     })
-
-    // pool.query("SELECT song_title FROM mySongs;", (err, res) => {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log(res.rows[0]);
-    //     }
-    // })
-    //     } else {
-    //         console.log(res.rows[0])
-    //     }
-    // }
-    //})
-
-    res.render('mySongs');
 }
 
+function goToFav(req, res) {
+    pool.query("SELECT song_title, author, lyrics FROM mySongs;", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var rows = result.rows;
+            res.render('mySongs', { params: JSON.stringify(rows) });
+        }
+    })
+}
 
-// pool.query(sql, function(err, result) {
-//     // If an error occurred...
-//     if (err) {
-//         console.log("Error in query: ")
-//         console.log(err);
-//     }
-
-//     // Log this to the console for debugging purposes.
-//     console.log("Back from DB with result:");
-//     console.log(result.rows);
-
-// });
-
-
-
-// pool.connect((err, client, done) => {
-//     if (err) throw err
-//     client.query('SELECT * FROM mySongs;', (err, res) => {
-//         done()
-//         if (err) {
-//             console.log(err.stack)
-//         } else {
-//             console.log(res.rows[0])
-//         }
-//     })
-// })
-// // promise - checkout a client
-// pool.connect().then(client => {
-//     return client
-//         .query('SELECT author FROM mySongs;')
-//         .then(res => {
-//             client.release()
-//             console.log(res.rows[0])
-//         })
-//         .catch(err => {
-//             client.release()
-//             console.log(err.stack)
-//         })
-// })
-//     // async/await - check out a client
-//     ; (async () => {
-//         const client = await pool.connect()
-//         try {
-//             const res = await client.query('SELECT * FROM mySongs;')
-//             console.log(res.rows[0])
-//         } finally {
-//             // Make sure to release the client before any error handling,
-//             // just in case the error handling itself throws an error.
-//             client.release()
-//         }
-//     })().catch(err => console.log(err.stack))
